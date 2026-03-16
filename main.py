849a68fe120c1b36b064d5dbee38a8b0ed3a3c0f
BraisGlezArias <brais.gonzalez.arias@gmail.com>
2026-03-16 17:36:05 +0100

    Creación y muestra de usuarios

---
+++ main.py
@@ -0,0 +1,21 @@
+from faker import Faker
+
+fake = Faker()
+
+codigos = set(list(range(1, 16)))
+
+usuarios = dict([])
+
+for codigo in codigos:
+    datos = dict(
+        [
+            ("nome", fake.name()),
+            ("dirección", fake.address()),
+            ("correo electrónico", fake.email()),
+            ("teléfono", fake.phone_number()),
+        ]
+    )
+
+    usuarios[codigo] = datos
+
+print(usuarios)
