60d07de3fa995230da30b71c0f46a9f4a42e65ba
BraisGlezArias <brais.gonzalez.arias@gmail.com>
2026-03-16 18:37:26 +0100

    Añadidos comentarios

--- main.py
+++ main.py
@@ -1,29 +1,39 @@
 from faker import Faker
 import random
 
+# Configuramos la función Faker para que devuelva los datos en castellano.
+# También aprovechamos a guardarla en una variable para mayor comodidad.
 fake = Faker("es_ES")
 
+# Los códigos de los usuarios no pueden repetirse.
+# Para ello, creamos un set de los números del 1 al 15.
 codigos = set(list(range(1, 16)))
 
 usuarios = dict([])
 
 for codigo in codigos:
+    # Creamos un diccionario que almacene los datos generados por Faker().
     datos = {
-            "nome": fake.name(),
-            "dirección": fake.address(),
-            "correo electrónico": fake.email(),
-            "teléfono": fake.phone_number()
+        "nome": fake.name(),
+        "dirección": fake.address(),
+        "correo electrónico": fake.email(),
+        "teléfono": fake.phone_number(),
     }
 
+    # Asociamos dichos datos al código correspondiente.
     usuarios[codigo] = datos
 
     print(
-        f"\nUsuario {codigo}: {usuarios[codigo]['nome']} | "
-        f"{usuarios[codigo]['dirección']} | "
-        f"{usuarios[codigo]['correo electrónico']} | "
-        f"{usuarios[codigo]['teléfono']}"
+        f"""\nUsuario {codigo}: {usuarios[codigo]["nome"]} | 
+        {usuarios[codigo]["dirección"]} | 
+        {usuarios[codigo]["correo electrónico"]} | 
+        {usuarios[codigo]["teléfono"]}"""
     )
 
+# Seleccionamos un valor aleatorio de entre los códigos.
 ganador = random.choice(list(codigos))
 
 print(f"\nO usuario chamado {usuarios[ganador]['nome']} foi o afortunado!\n")
+
+# Debes desenvolver o programa empregando Git e GitHub.
+# Repositorio: https://github.com/BraisGlezArias/tarefa2_brais_gonzalez_Arias.git
